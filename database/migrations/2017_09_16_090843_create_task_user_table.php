1d3abbf6e7b83f34d0e8c47be8d6b439b5f72a06
ikikika <iki.jito@gmail.com>
2017-09-16 17:23:54 +0800

    fix error for specofied key too long, import facades schema class

--- database/migrations/2017_09_16_090843_create_task_user_table.php
+++ database/migrations/2017_09_16_090843_create_task_user_table.php
@@ -19,9 +19,9 @@ class CreateTaskUserTable extends Migration
             $table->integer('task_id')->unsigned();
             $table->integer('user_id')->unsigned();
 
-            $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('task_id')->references('id')->on('tasks');
-
+            $table->foreign('user_id')->references('id')->on('users');
+            
             $table->timestamps();
         });
     }
