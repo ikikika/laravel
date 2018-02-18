0d79badd90e056c8f062c10a9792dc40aadfc06a
ikikika <iki.jito@gmail.com>
2018-02-19 01:40:12 +0800

    vue component progress bar

--- resources/assets/js/components/OrderProgress.vue
+++ resources/assets/js/components/OrderProgress.vue
@@ -1,9 +1,16 @@
 <template>
-    <div>Order Progress Component</div>
+    <div class="progress">
+      <progressbar now="50" type="success" striped animated></progressbar>
+    </div>
 </template>
 
 <script>
+    import { progressbar } from 'vue-strap'
+
     export default {
+        components: {
+          progressbar
+        },
         mounted() {
             console.log('Component mounted.')
         }
