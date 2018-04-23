e4789b11018626326c513ca2ee0f1d6a3d67706e
ikikika <iki.jito@gmail.com>
2018-04-23 15:52:43 +0800

    http://localhost:8000/api/products returns product details but its very bad because it shows info we dun want others to see

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources\Product;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductResource extends JsonResource
+class ProductResource extends Resource
 {
     /**
      * Transform the resource into an array.
@@ -14,6 +14,17 @@ class ProductResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+      return [
+        'name' => $this->name,
+          'description' => $this->detail,
+          'price' => $this->price,
+          'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+          'discount' =>$this->discount,
+          'totalPrice' => round(( 1 - ($this->discount/100)) * $this->price,2),
+          'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
+          'href' => [
+              'reviews' => route('reviews.index',$this->id)
+          ]
+      ];
     }
 }
