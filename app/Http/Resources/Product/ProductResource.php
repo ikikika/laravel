929ac4e582274ccf6ba309626cccbc99c315d4c9
ikikika <iki.jito@gmail.com>
2018-04-23 16:05:23 +0800

    modified product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,14 +15,14 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
       return [
-        'name' => $this->name,
+          'name' => $this->name,
           'description' => $this->detail,
           'price' => $this->price,
-          'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+          'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock, //
           'discount' =>$this->discount,
-          'totalPrice' => round(( 1 - ($this->discount/100)) * $this->price,2),
-          'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
-          'href' => [
+          'totalPrice' => round(( 1 - ($this->discount/100)) * $this->price,2), //
+          'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet', //
+          'href' => [ //
               'reviews' => route('reviews.index',$this->id)
           ]
       ];
