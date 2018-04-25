acb5128173d5eab92ab861d7ea64be68c6abc2ca
ikikika <iki.jito@gmail.com>
2018-04-26 01:36:54 +0800

    if accept json, return response, else if access from browser, return html page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,9 +47,12 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+      if( $request->expectsJson() ){
         if( $exception instanceof ModelNotFoundException ){
           return response('Model not found', 404);
         }
+      }
+
         return parent::render($request, $exception);
     }
 }
