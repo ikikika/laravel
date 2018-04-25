ea72699bdacef06eeaf93d77b644318ea22605f9
ikikika <iki.jito@gmail.com>
2018-04-26 02:14:39 +0800

    throw exception when user trying to edit pdt not same as logged in user

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -84,6 +84,7 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
+        $this->ProductUserCheck($product);
         //return $request->all();
         $request['detail'] = $request->description;
         unset( $request['description'] );
@@ -101,7 +102,15 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
+        $this->ProductUserCheck($product);
         $product->delete();
         return response(null,Response::HTTP_NO_CONTENT);
+    }
+
+    public function ProductUserCheck($product)
+    {
+        if( Auth::id() !== $product->user_id ){
+          throw new ProductNotBelongsToUser;
+        }
     }
 }
