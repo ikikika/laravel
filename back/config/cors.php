4a548d627c88b3be1f0e6ef2268ecd3bf436f04c
ikikika <iki.jito@gmail.com>
2018-05-04 08:18:31 +0800

    remove cors error, composer require barryvdh/laravel-cors, php artisan vendor:publish --provider="Barryvdh\Cors\ServiceProvider"

---
+++ back/config/cors.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+    */
+
+    'supportsCredentials' => false,
+    //'allowedOrigins' => ['http://localhost:8080'], limit access
+    'allowedOrigins' => ['*'],
+    'allowedOriginsPatterns' => [],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+
+];
