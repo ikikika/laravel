8251f3509643d93deac3ba845920c907e3c9222f
ikikika <iki.jito@gmail.com>
2018-05-11 09:59:31 +0800

    customer seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          //$this->call(UsersTableSeeder::class);
-         factory(\App\User::class, 10)->create();
+         //factory(\App\User::class, 10)->create();
+         factory(\App\Customer::class, 10)->create();
+
     }
 }
