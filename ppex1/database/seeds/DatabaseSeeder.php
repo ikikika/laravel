c36d7b35a79d3c52f84495d7dd9fe9a377dc15f7
ikikika <iki.jito@gmail.com>
2018-10-15 22:16:46 +0800

    ppex1 db seed

--- ppex1/database/seeds/DatabaseSeeder.php
+++ ppex1/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+      DB::table('users')->insert([
+          'name' => 'test admin',
+          'email' => 'a@a.com',
+          'password' => bcrypt('a@a.com'),
+      ]);
     }
 }
