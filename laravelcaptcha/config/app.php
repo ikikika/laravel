4447cdb57b1d37ce3b732f9e56762f56c6270842
ikikika <iki.jito@gmail.com>
2018-10-23 10:17:18 +0800

    captcha Add Service Provider and aliases

--- laravelcaptcha/config/app.php
+++ laravelcaptcha/config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Mews\Captcha\CaptchaServiceProvider::class,
     ],
 
     /*
@@ -223,6 +224,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Captcha' => Mews\Captcha\Facades\Captcha::class,
 
     ],
 
