fce429125fc335efd16a6e949fe4fe203a79981e
ikikika <iki.jito@gmail.com>
2018-10-23 10:22:42 +0800

    captcha define routes

--- laravelcaptcha/routes/web.php
+++ laravelcaptcha/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('createcaptcha', 'CaptchaController@create');
+Route::post('captcha', 'CaptchaController@captchaValidate');
+Route::get('refreshcaptcha', 'CaptchaController@refreshCaptcha');
