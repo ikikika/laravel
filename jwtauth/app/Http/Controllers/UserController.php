8fbe932511ef223d6c3fc4f603f4b62f653ad365
ikikika <iki.jito@gmail.com>
2019-06-11 20:33:21 +0800

    user controller get auth user function

--- jwtauth/app/Http/Controllers/UserController.php
+++ jwtauth/app/Http/Controllers/UserController.php
@@ -45,12 +45,29 @@ class UserController extends Controller
         $credentials = $request->json()->all();
 
         try{
-            if( ! $token == JWTAuth::attempt($credentials) ){
+            if( !$token == JWTAuth::attempt($credentials) ){
                 return response()->json( ['error'=>'invalid_credentials'], 400);
             }
         } catch( JWTException $e){
             return response()->json( ['error'=>'could_not_create_token'], 500);
         }
         return response()->json(compact('token'), 201);
+    }
+
+    public function getAuthenticatedUser()
+    {
+        try{
+            if( !$user == JWTAuth::parseToken()->authenticate() ){
+                return response()->json(['user_not_found'],400);
+            }
+        } catch( Tymon\JWTAuth\Exception\TokenExpiredException $e ) {
+            return response()->json(['token_expired'], $e->getStatusCode());
+        } catch( Tymon\JWTAuth\Exception\TokenInvalidException $e ) {
+            return response()->json(['token_invalid '], $e->getStatusCode());
+        } catch( Tymon\JWTAuth\Exception\JWTException $e ) {
+            return response()->json(['token_absent'], $e->getStatusCode());
+        }
+
+        return response()->json(compact('user'));
     }
 }
